314346d35d2e08e523bbd6f9eca349ecb6b59340
Ali <AliNazarii7394@gmail.com>
2023-09-18 10:54:26 +0330

    list stp2 Location selection, Map component, added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -7,6 +7,8 @@ import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
+import Map from "../Map";
 
 enum STEPS {
   CATEGORY = 0,
@@ -98,12 +100,21 @@ const RentModal = () => {
       </div>
     </div>
   );
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='Where is your place located?' subtitle='Help guests find you!' />
+        <CountrySelect value={location} onChange={(value) => setCustomValue("location", value)} />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
 
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       title='Airbnb yor Home'
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
