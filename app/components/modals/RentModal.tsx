5c0a9475d33d7b22362ca313b62827977d3e7118
Ali <AliNazarii7394@gmail.com>
2023-09-18 11:40:14 +0330

    list stp2 Location selection, Map component added

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -5,8 +5,10 @@ import Modal from "./Modal";
 import useRentModal from "@/app/hooks/useRentModal";
 import Heading from "../Heading";
 import { categories } from "../navbar/Categories";
+import dynamic from "next/dynamic";
 import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
+import CountrySelect from "../inputs/CountrySelect";
 
 enum STEPS {
   CATEGORY = 0,
@@ -41,13 +43,20 @@ const RentModal = () => {
       description: "",
     },
   });
-
   const location = watch("location");
   const category = watch("category");
   const guestCount = watch("guestCount");
   const roomCount = watch("roomCount");
   const bathroomCount = watch("bathroomCount");
   const imageSrc = watch("imageSrc");
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
 
   const setCustomValue = (id: string, value: any) => {
     setValue(id, value, {
@@ -84,7 +93,15 @@ const RentModal = () => {
   let bodyContent = (
     <div className='flex flex-col gap-8'>
       <Heading title='Which of these best describes your place?' subtitle='Pick a category' />
-      <div className='grid grid-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto'>
+      <div
+        className='
+          grid 
+          grid-cols-1 
+          md:grid-cols-2 
+          gap-3
+          max-h-[50vh]
+          overflow-y-auto
+        '>
         {categories.map((item) => (
           <div key={item.label} className='col-span-1'>
             <CategoryInput
@@ -99,11 +116,21 @@ const RentModal = () => {
     </div>
   );
 
+  if (step === STEPS.LOCATION) {
+    bodyContent = (
+      <div className='flex flex-col gap-8'>
+        <Heading title='Where is your place located?' subtitle='Help guests find you!' />
+        <CountrySelect value={location} onChange={(value) => setCustomValue("location", value)} />
+        <Map center={location?.latlng} />
+      </div>
+    );
+  }
+
   return (
     <Modal
       isOpen={rentModal.isOpen}
       onClose={rentModal.onClose}
-      onSubmit={rentModal.onClose}
+      onSubmit={onNext}
       title='Airbnb yor Home'
       actionLabel={actionLabel}
       secondaryActionLabel={secondaryActionLabel}
