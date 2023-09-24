872d3eaa6a934952c4a72db139e9f9915e13f73f
Ali <AliNazarii7394@gmail.com>
2023-09-24 21:48:41 +0330

    Reservation functionality add&disable reserved Day

--- app/listings/[listingId]/page.tsx
+++ app/listings/[listingId]/page.tsx
@@ -5,15 +5,15 @@ import ClientOnly from "@/app/components/ClientOnly";
 import EmptyState from "@/app/components/EmptyState";
 
 import ListingClient from "./ListingClient";
+import getReservations from "@/app/actions/getReservations";
 
 interface IParams {
   listingId?: string;
 }
 
 const ListingPage = async ({ params }: { params: IParams }) => {
-
   const listing = await getListingById(params);
-  
+  const reservations = await getReservations(params);
   const currentUser = await getCurrentUser();
 
   if (!listing) {
@@ -26,7 +26,7 @@ const ListingPage = async ({ params }: { params: IParams }) => {
 
   return (
     <ClientOnly>
-      <ListingClient listing={listing} currentUser={currentUser} />
+      <ListingClient listing={listing} currentUser={currentUser} reservations={reservations} />
     </ClientOnly>
   );
 };
