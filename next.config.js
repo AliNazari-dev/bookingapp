7fb645d1dd53d035fa0683336001448504a83b10
Ali <AliNazarii7394@gmail.com>
2023-09-27 02:18:14 +0330

    bug fixed

--- next.config.js
+++ next.config.js
@@ -4,8 +4,12 @@ const nextConfig = {
     appDir: true,
   },
   images: {
-    domains: ["res.cloudinary.com", "avatars.githubusercontent.com", "lh3.googleusercontent.com"],
-  },
-};
+    domains: [
+      'res.cloudinary.com', 
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com'
+    ]
+  }
+}
 
-module.exports = nextConfig;
+module.exports = nextConfig
